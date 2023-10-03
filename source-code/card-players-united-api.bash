85979f7cd40c875956d3cc31ac24422d726fbbb5
Software Shinobi <troy@softwareshinobi.online>
2023-10-02 19:39:16 -0500

    automated terminal push

--- source-code/card-players-united-api.bash
+++ source-code/card-players-united-api.bash
@@ -4,4 +4,14 @@ reset
 
 clear
 
-python3 card-players-united-api.py
+set -e
+
+set -x
+
+##python3 card-players-united-api.py
+
+git pull;
+
+./build-pass-1-build-docker-image-from-dockerfile.sh ;
+
+./execute-pass-1-run-local-environment.sh 
