32a723da60baa0153b952fdf7a2325a02624565a
Software Shinobi <troy@softwareshinobi.online>
2023-10-02 19:52:29 -0500

    automated terminal push

--- source-code/DjangoAPI/urls.py
+++ source-code/DjangoAPI/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.conf.urls import re_path,include
+from django.urls import include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('admin/', admin.site.urls),
     re_path(r'^',include('EmployeeApp.urls'))
 ]
